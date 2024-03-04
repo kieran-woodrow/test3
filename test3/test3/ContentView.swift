1a0751d08c631eb43a689e6b9ac762606ce1a598
kieranwoodrow <kwoodrow@tracker.co.za>
2024-03-04 14:56:58 +0200

    setup done

---
+++ test3/test3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test3
+//
+//  Created by Kieran Woodrow on 2024/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
